0c52afc17b1f4f6af29a25dbdfb827db50342a09
djra26111990 <djra26111990@gmail.com>
2020-08-03 16:21:56 -0500

    fix bcrypt import

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dbConfig = require('./database/db');
+//const bcrypt = require("bcrypt");
+
 
 // Express APIs
 const api = require('./routes/auth.routes');
